53b4759434e155a4687ee80ba9894a4318ef1899
xavier <anggaez117@gmail.com>
2021-01-29 03:03:04 +0700

    v1.1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4004;
+const PORT = process.env.PORT || 4004;
 const router = require("./routes");
 const session = require('express-session')
 
